cd74a4db95f0459acd580e7ef195705e58d82f92
aaccurso <accurso.alan@gmail.com>
2015-07-06 13:03:52 -0300

    feat(GameController): creates GameController to handle singletons

---
+++ Assets/Resources/Scripts/GameController/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+	public List<Singleton> singletons;
+
+	const string CLONE_SUFFIX = "(Clone)";
+
+	// Use this for initialization
+	void Start ()
+	{
+		InstantiateSingletons ();
+	}
+
+	void InstantiateSingletons ()
+	{
+		singletons.ForEach ((singleton) => {
+			// Instantiate only if singleton not found
+			if (!GameObject.Find (singleton.name + CLONE_SUFFIX)) {
+				Instantiate (singleton);
+			}
+		});
+	}
+
+	public void GoToLevel (string level)
+	{
+		Application.LoadLevel (level);
+	}
+}
---
+++ Assets/Resources/Scripts/GameController/PlayerSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSettings : Singleton
+{
+	// Use this for initialization
+	void Start ()
+	{
+		// TODO: implement
+	}
+}
